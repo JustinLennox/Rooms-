e1819aa25d15f2e8b5f3f244827b47d87fb77c49
Justin Lennox <justinanthonylennox@gmail.com>
2015-09-30 19:11:07 -0400

    Initial Commit

---
+++ Rooms!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rooms!
+//
+//  Created by Justin Lennox on 9/30/15.
+//  Copyright © 2015 Justin Lennox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

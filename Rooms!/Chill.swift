cf242c520520271fa1259df16be8ee2ae61ebac3
Justin Lennox <justinanthonylennox@gmail.com>
2015-11-12 02:03:37 -0500

    Before Messing with And Chill

---
+++ Rooms!/Chill.swift
@@ -0,0 +1,19 @@
+//
+//  Chill.swift
+//  Rooms!
+//
+//  Created by Justin Lennox on 11/11/15.
+//  Copyright © 2015 Justin Lennox. All rights reserved.
+//
+
+import Foundation
+
+class Chill{
+    var type : String
+    var details : String
+    
+    init(typeString : String, detailsString: String){
+        type = typeString
+        details = detailsString
+    }
+}

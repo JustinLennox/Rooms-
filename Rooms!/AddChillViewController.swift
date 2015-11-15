6ce24b4b1f9ed8f6c253f9dd53dad8d2418e2bd8
Justin Lennox <justinanthonylennox@gmail.com>
2015-11-15 02:32:00 -0500

    Before Restructuring

---
+++ Rooms!/AddChillViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddChillViewController.swift
+//  Rooms!
+//
+//  Created by Justin Lennox on 11/15/15.
+//  Copyright © 2015 Justin Lennox. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class AddChillViewController: UIViewController {
+
+}
